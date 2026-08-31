39ef5060af09e845c23215efd7c6e8d6b4f72548
Jingting Liu <jingtingliu123@gmail.com>
2026-08-31 13:05:35 -0700

    Time: 207 ms (39.37%), Space: 20 MB (6.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,18 +1,45 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        charSet = set()
-        l = 0
-        res = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        #abcabcbb
+        #a --> ab --> abc --> abca
+        # I want to start from the beginning to remove the duplicate if it is the same, and I need a way to know whether there is still duplicate
+        # set length comparison tells me whether there are still duplicates
+        # to start from the beginning of the substring I need a way to track the index of the beginning
+        # everytime before I update I need to update the max first
 
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            res = max(res, len(charSet))
-        return res
+        # Solution 1
+        # result = 0
+        # substring = ""
+
+        # for i in range(len(s)):
+        #     # remove duplicates
+        #     if s[i] in substring:
+        #         #print("currently there is " + s[i] + " in " + substring)
+        #         result = max(result,len(substring))
+        #         while s[i] in substring: 
+        #             #print("removing " + substring[0])
+        #             substring = substring[1:]
+        #         #print("now there is only " + substring)
+            
+        #     substring += s[i]
+        #     #print("added " + s[i] + " to make " + substring)
+        
+        # result = max(result,len(substring))
+        # return result
+
+        seen = set()
+        left = 0
+        result = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            
+            seen.add(s[right])
+            result = max(result, right - left + 1)
+        
+        return result
+
+
         
