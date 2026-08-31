1ad2a9ecc60afeed12773b4c16f1bf803db7f1b8
Jingting Liu <jingtingliu123@gmail.com>
2026-08-31 14:02:15 -0700

    Time: 17 ms (16.6%), Space: 19.3 MB (80.54%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,24 +1,29 @@
-class Solution(object):
-    def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
-        
-        # base case
-        if (numRows == 1):
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        buckets = [""] * numRows
+        row = 0
+        direction = 1 # Rule: 1 is up, -1 is down
+
+        if numRows == 1:
             return s
-        
-        res = ""
-        # we want to go down row by row
-        for row in range (numRows):
-            increment = (numRows - 1) * 2
 
-            for index in range(row, len(s) ,increment):
-                res += s[index]
+        for i in range(len(s)):
 
-                if(row != 0    and     row != numRows-1     and     index + increment - row*2 < len(s)):
-                    res += s[index + increment - row*2]
+            # determine the direction of the zigzag
+            if row == 0:
+                direction = 1
+            elif row == numRows-1:
+                direction = -1
+            
+            # add to the back of the string
+            buckets[row] += s[i]
+            # iterate through the rows
+            row += direction
         
-        return res
+        result = ""
+        for row in range(numRows):
+            result += buckets[row]
+        return result
+            
+
+
