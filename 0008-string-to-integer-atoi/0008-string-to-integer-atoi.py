3afe1abdcb5f52488b538094130b50299e76bf92
Jingting Liu <jingtingliu123@gmail.com>
2026-08-31 17:15:09 -0700

    Time: 3 ms (36.31%), Space: 19.5 MB (6.06%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,44 +1,48 @@
-class Solution(object):
-    def myAtoi(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        string_int = ""
-        sign = False
-        digit_start = False
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        sign = 1
+        left = 0
+        result = 0
 
-        for index in range(len(s)):
-
-            if (string_int == "" and sign!= True):
-                if s[index].isspace():
-                    continue
-                elif s[index] == "-":
-                    sign = True
-                    string_int = "-"
-                elif s[index] == "+":
-                    sign = True
-                    pass
-                elif s[index].isdigit():
-                    digit_start = True
-                    string_int += s[index]
-                else:
-                    break
-            else:
-                if not s[index].isdigit():
-                    break
-                # once digits started, append ALL digits (including 0)
-                string_int += s[index]
-
-        if string_int == "-" or string_int == "":
+        # base case
+        if len(s) == 0:
             return 0
 
-        num = int(string_int)
-        INT_MIN = -(2**31)
-        INT_MAX = (2**31) - 1
+        # step 1 
+        for i in range(len(s)):
+            if s[i] == " ":
+                continue
+            else:
+                left = i
+                break
+        
+        # step 2
+        if s[left] == "-":
+            left += 1
+            sign = -1
+        elif s[left] == "+":
+            left += 1
+            sign = 1
+        
+        # step 3
+        for i in range(left,len(s),1):
+            if s[i].isdigit():
+                result = result * 10 + (ord(s[i]) - ord('0'))
+            else:
+                break
+        
+        # step 4
+        result = result * sign
 
-        if num < INT_MIN:
-            return INT_MIN
-        if num > INT_MAX:
-            return INT_MAX
-        return num
+        if result < 0:
+            result = max(result, -2**31)
+        else:
+            result = min(result, 2**31 - 1)
+        
+        return result
+        
+
+            
+
+
+
